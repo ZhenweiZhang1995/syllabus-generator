c977036b60fceee3224d11a7b05ba8a9d69ee50d
ZhenweiZhang1995 <apple@dhcp-v880-20-00927.1Xwireless.unc.edu>
2017-02-13 16:15:12 -0500

    Create new paper/assignment/project
    Create new paper/assignment/project;
    
    Create new item will now appear only when user click “Create New”
    button.

--- src/components/additional.vue
+++ src/components/additional.vue
@@ -35,23 +35,20 @@
       <transition name="fade">
         <div v-show ="!item.finished">
           <div>
-            <label class="label">{{ item.itemTitle }}</label>
+            <!-- <label class="label">{{ item.itemTitle }}</label> -->
+            <input type="text" class="input center-input" placeholder="Item Title" v-model ="item.itemTitle"/>
           </div>
-            <textarea class="textarea"  v-model ="item.itemContent"></textarea>
+            <textarea class="textarea"  placeholder="Item Content" v-model ="item.itemContent"></textarea>
             <div class="control">
               <a class="button is-primary" @click="done(item)">Delete</a>
             </div>
         </div>
       </transition>
     </template>
-    
+
     </section>
 
-
-
-    <br>
-    <br>
-    <section>
+    <!-- <section>
       <label class="label">Add new item</label>
       <div class="control is-grouped">
         <div class="control is-expanded">
@@ -67,7 +64,11 @@
       <div class="control">
         <a class="button" @click="cancel">Cancel</a>
       </div>
-    </section>
+    </section> -->
+
+    <p class="control has-addons has-addons-centered">
+      <a class="button is-primary is-medium" @click="addItem">Add New Item</a>
+    </p>
 
 
 
@@ -101,8 +102,8 @@ export default {
       newFavorite: false,
       editing: false,
       items: [{
-        itemTitle: "Watch a movie",
-        itemContent: "Zootopia on Sunday 6pm ",
+        itemTitle: "Seek for help",
+        itemContent: "Your can seek for help from me anytime by e-mail/by appointment",
         finished: false,
         editing: false
       }]
@@ -124,45 +125,11 @@ export default {
         })
         this.newItemTitle = '';
         this.newItemContent = '';
-        this.newFavorite = false;
       },
       done(i) {
         i.finished = true;
       },
-      editBtn(i) {
-        // i.itemTitle = "";
-        // i.itemContent = "";
-        i.editing = true;
-      },
-      editStatus(i) {
-        return i.editing;
-      },
-      finishEditBtn(i) {
-        i.editing = false;
-      }
   },
-
-  // methods: {
-  //   cancel() {
-  //       this.newItemTitle = '';
-  //       this.newItemContent = '';
-  //     },
-  //   addItem() {
-  //       this.items.push({
-  //       itemTitle: this.newItemTitle,
-  //       itemContent: this.newItemContent,
-  //       })
-  //       this.newItemTitle = '';
-  //       this.newItemContent = '';
-  //       finished: false;
-  //     },
-  //   delete(i) {
-  //       i.finished = true;
-  //     },
-  // },
-
-
-
 
 
 
@@ -173,5 +140,8 @@ export default {
   .additional{
     padding-left: 20%;
     padding-right: 20%;
+  }
+  .center-input{
+    text-align:center;
   }
 </style>
--- src/components/assignment.vue
+++ src/components/assignment.vue
@@ -6,20 +6,66 @@
       <textarea class="textarea" placeholder="Assignment Description" v-model ="AssignmentDescription">
       </textarea>
     </div>
+    <br>
+    <br>
+
+    <section>
+    <template v-for="item in items">
+      <transition name="fade">
+        <div v-show ="!item.finished">
+
+            <div class="control is-grouped">
+                  <span class="select">
+                    <select>
+                      <option>Assignment</option>
+                      <option>Paper</option>
+                      <option>Project</option>
+                    </select>
+                  </span>
+              <input class="input" type="text" placeholder="Number" v-model ="item.assignNum">
+              <input class="input is-expanded" type="text" placeholder="Description" v-model ="item.itemDescription">
+              <!-- <p class="control">
+                <a class="button is-primary" @click="addItem">+</a>
+              </p> -->
+            </div>
+            <p class="control">
+              <textarea class="textarea" placeholder="Requirments" v-model ="item.itemContent"></textarea>
+            </p>
+            <div class="control">
+              <a class="button is-primary" @click="done(item)">Delete</a>
+            </div>
+        </div>
+      </transition>
+    </template>
+
+    </section>
+
+  <!-- <form class="addProject">
+    <br>
+    <div class="control is-grouped">
+          <span class="select">
+            <select>
+              <option>Assignment</option>
+              <option>Paper</option>
+              <option>Project</option>
+            </select>
+          </span>
+      <input class="input" type="text" placeholder="Number" v-model ="newAssignNum">
+      <input class="input is-expanded" type="text" placeholder="Description" v-model ="newItemDescription">
+      <p class="control">
+        <a class="button is-primary" @click="addItem">+</a>
+      </p>
+    </div>
+    <p class="control">
+      <textarea class="textarea" placeholder="Requirments" v-model ="newItemContent"></textarea>
+    </p>
+  </form> -->
 
     <p class="control has-addons has-addons-centered">
-      <span class="select">
-        <select>
-          <option>Assignment</option>
-          <option>Paper</option>
-          <option>Project</option>
-        </select>
-      </span>
-      <input class="input" type="text" placeholder="Number">
-      <a class="button is-primary">
-        Transfer
-      </a>
+      <a class="button is-primary is-medium" @click="addItem">Add New Assignment/Paper/Project</a>
     </p>
+
+
 
   </div>
 </template>
@@ -29,8 +75,42 @@ export default {
   data(){
     return{
       AssignmentDescription:"",
+      newAssignNum:'',
+      newItemDescription:'',
+      newItemContent:'',
+      finished: false,
+      editing: false,
+      items: [{
+        assignNum:"1",
+        itemDescription:"Write an essay about UNC in general",
+        itemContent:"This should be 10k words long,single space",
+        finished: false,
+        editing: false,
+      }]
     }
-  }
+  },
+  methods: {
+    cancel() {
+        this.newAssignNum = '';
+        this.newItemDescription = '';
+        this.newItemContent = '';
+      },
+      addItem() {
+        this.items.push({
+          assignNum:this.newAssignNum,
+          itemDescription: this.newItemDescription,
+          itemContent: this.newItemContent,
+          finished: false,
+          editing: false
+        })
+        this.newAssignNum = '';
+        this.newItemDescription = '';
+        this.newItemContent = '';
+      },
+      done(i) {
+        i.finished = true;
+      },
+  },
 }
 </script>
 
